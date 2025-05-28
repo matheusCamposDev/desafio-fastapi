42acfd6a948553921065a8a6e6ec5e34587280dc
Matheus Campos <theuscampos45@gmail.com>
2025-05-28 19:31:04 -0300

    Removido função create_engine

--- app/db/session.py
+++ app/db/session.py
@@ -8,12 +8,9 @@ from app.models.order import Order
 
 load_dotenv()
 
-
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
-SQLModel.metadata.create_all(engine)
-
 
 def get_session():
     with Session(engine) as session:
